f2e6a82e43e7b9ea3710cc450d01f801607985b3
Jully Khorolska <uxa0608@gmail.com>
2023-05-13 21:10:37 +0300

    creation classes User and CheckUser

--- src/checkUser/CheckUser.java
+++ src/checkUser/CheckUser.java
@@ -1,2 +1,80 @@
-package checkUser;public class CheckUser {
+package checkUser;
+import user.User;
+
+public class CheckUser {
+    public static void main(String[] args) {
+
+        User firstUser = new User();
+        User secondUser = new User("Vadim");
+        User thirdUser = new User("Jon", "099999a");
+
+//        information about first user
+
+        firstUser.userName = "Yulia";
+        System.out.println("\nInformation about: " + firstUser.userName);
+
+        firstUser.setAge(18);
+        System.out.println(firstUser.userName + " age: " + firstUser.getAge());
+
+        firstUser.setPassword("1234579A");
+        System.out.println(firstUser.userName + " password: " + firstUser.getPassword());
+
+        firstUser.email = "uxa@ukr.net";
+        System.out.println(firstUser.userName + " email: " + firstUser.email);
+        System.out.println("------------------------------------");
+
+        firstUser.makePurchase(0); // sum of purchase by first user
+        firstUser.makePurchase(10);
+        firstUser.makePurchase(100);
+
+        System.out.println("------------------------------------");
+        firstUser.purchaseOfUserPrint();
+        System.out.println("\n");
+
+//        information about second user
+
+        System.out.println("Information about: " + secondUser.userName);
+
+        secondUser.setAge(17);
+        System.out.println(secondUser.userName + " age: " + secondUser.getAge());
+
+        secondUser.setPassword("A1111111");
+        System.out.println(secondUser.userName + " password: " + secondUser.getPassword());
+
+        secondUser.email = "vadim@gmail.com";
+        System.out.println(secondUser.userName + " email: " + secondUser.email);
+        System.out.println("------------------------------------");
+
+        secondUser.makePurchase(20.9);
+        secondUser.makePurchase(20.0);
+        System.out.println("------------------------------------");
+        secondUser.purchaseOfUserPrint();
+
+        System.out.println("\n");
+
+//        information about third user
+
+        System.out.println("Information about: " + thirdUser.userName);
+
+        thirdUser.setAge(-70);
+        System.out.println(thirdUser.userName + " age: " + thirdUser.getAge());
+
+        System.out.println(thirdUser.userName + " password: " + thirdUser.getPassword());
+
+        thirdUser.email = "jon1234@gmail.com";
+        System.out.println(thirdUser.userName + " email: " + thirdUser.email);
+        System.out.println("------------------------------------");
+
+        thirdUser.makePurchase(0);
+        thirdUser.makePurchase(100);
+        thirdUser.makePurchase(-80);
+
+        System.out.println("------------------------------------");
+        thirdUser.purchaseOfUserPrint();
+
+        System.out.println("\n");
+        User.printTotalAmountOfSpentMoney();
+
+    }
 }
+
--- src/user/User.java
+++ src/user/User.java
@@ -1,2 +1,85 @@
-package user;public class User {
+package user;
+public class User {
+    public User (){
+    }
+    public User (String userName){
+
+        this.userName = userName;
+    }
+    public User (String userName, String password){
+        this.userName = userName;
+        this.password = password;
+    }
+    public String userName;
+    private  int age;
+    private String password;
+    public String email;
+    static boolean isActive;
+    static public double amountSpentMoney = 0;
+    public double purchasePrice = 0;
+
+    public int getAge() {
+        return age;
+    }
+
+    // set-function for user's age validation
+    public void setAge(int age) {
+        if(age > 0 ){
+            if(age < 18){
+                this.age = age;
+                System.out.println(this.age + " years" + " - is less than 18." + " You can't make purchase");
+            } else this.age = age;
+        } else {
+            this.age = age;
+            System.out.println(this.age + " years" + " - isn't valid." + " You must enter your age!");
+        }
+    }
+    public String getPassword() {
+
+        return password;
+    }
+
+    // set-function for user's password validation
+    public void setPassword(String password) {
+
+        if(!password.isEmpty()) {
+            if(!password.contains(" ")){
+                if (password.length() != 8 ) {
+                    System.out.println("Password must contain 8 symbols");
+                } else if (!password.matches("(.*)[0-9](.*)")){
+                    System.out.println("Password must contain numbers");
+                } else if (password.equals(password.toLowerCase())){
+                    System.out.println("Password must contain not less one uppercase symbol");
+                } else this.password = password;
+            } else System.out.println("Please, enter password without backspaces");
+        } else System.out.println("Please, enter password with 8 symbols");
+    }
+
+    //  function that counts sum of purchase by one user and total sum of purchase by all users
+    public void makePurchase(double purchasePrise){
+        if(purchasePrise >= 0){
+            User.amountSpentMoney += purchasePrise;
+            this.purchasePrice += purchasePrise;
+            if(User.amountSpentMoney >= 100){
+                isActive = true;
+            }
+            System.out.println(this.userName + " made purchase on: " + purchasePrise);
+            if(isActive){
+                System.out.println("You are active. Keep it up!");
+            } else {
+                System.out.println("You aren't active. Let's make another purchase!");
+            }
+        } else System.out.println("You purchase price is not valid " + purchasePrise);
+    }
+
+    //  function that displays total sum of purchase by the user
+    public void purchaseOfUserPrint(){
+        System.out.println(this.userName + " spent " + this.purchasePrice);
+    }
+
+    //  function that displays total sum of purchase by ALL users
+    static public void printTotalAmountOfSpentMoney(){
+        System.out.println("All users spent " + amountSpentMoney);
+    }
 }
+
